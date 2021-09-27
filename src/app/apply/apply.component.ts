62f4525c0749e0bfd7a2a3693ff495978a672121
nithish1199 <nithish2011@gmail.com>
2021-09-27 13:51:20 +0530

    small

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-apply',
   templateUrl: './apply.component.html',
   styleUrls: ['./apply.component.css']
 })
 export class ApplyComponent implements OnInit {
-
   PersonalDetails=new FormGroup({
     firstName:new FormControl("",[Validators.required]),
     lastName:new FormControl("",[Validators.required]),
     middleName:new FormControl(),
     Email:new FormControl("",[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    Password:new FormControl("", [Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")]),
-    ConfirmPassword:new FormControl("", [Validators.required]),
+    Password!:new FormControl("", [Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")]),
+    ConfirmPassword!:new FormControl("", [Validators.required]),
     MobileNumber:new FormControl("", [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
     DateofBirth:new FormControl(),
     Gender:new FormControl(),
@@ -23,6 +21,7 @@ export class ApplyComponent implements OnInit {
     AadharNumber:new FormControl(null,[Validators.required]),
     PanNumber:new FormControl(null,[Validators.required]),
   }
+  //{validators:this.passwordMatchValidator}
   );
   constructor() { }
 
@@ -74,4 +73,8 @@ export class ApplyComponent implements OnInit {
   {
     return this.PersonalDetails.get('PanNumber');
   }
+  passwordMatchValidator(PersonalDetails:FormGroup){
+    return PersonalDetails.controls['Password'].value===PersonalDetails!.controls['ConfirmPassword'].value?{'mismatch':false}:{'mismatch':true};
+  }
+ 
 }
