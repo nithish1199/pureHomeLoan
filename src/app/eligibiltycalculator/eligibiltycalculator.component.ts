762fdf2ca4768a52aaf35e0c87725bddcd0ba84b
nithish1199 <nithish2011@gmail.com>
2021-09-27 14:56:05 +0530

    eligibility

--- src/app/eligibiltycalculator/eligibiltycalculator.component.ts
+++ src/app/eligibiltycalculator/eligibiltycalculator.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LabelType, Options } from 'ng5-slider';
 @Component({
   selector: 'app-eligibiltycalculator',
   templateUrl: './eligibiltycalculator.component.html',
@@ -7,16 +7,152 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EligibiltycalculatorComponent implements OnInit {
 
-  input1!:number;
-  input2!:number;
-  input3!:number;
-  constructor() { }
+  // input1!:number;
+  // input2!:number;
+  // input3!:number;
+  filters: any;
+  pemi:any= {
+    value: "25"
+  }
+  remi:any = {
+    value: "8.5"
+  }
+  temi:any= {
+    value: "20"
+  }
+  memi:any = {
+    value: "240"
+  }
+
+  query = {
+    amount: "",
+    interest: "",
+    tenureYr: "",
+    tenureMo: ""
+  }
+
+  result = {
+    emi: "",
+    interest: "",
+    total: ""
+  }
+  yrToggel: boolean;
+  poptions: Options = {
+    floor: 1,
+    ceil: 200,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return value + '<b>L</b>';
+        case LabelType.High:
+          return value + '<b>L</b>';
+        default:
+          return value + '<b>L</b>';
+      }
+    }
+  };
+  roptions: Options = {
+    floor: 5,
+    ceil: 20,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return value + '<b>%</b>';
+        case LabelType.High:
+          return value + '<b>%</b>';
+        default:
+          return value + '<b>%</b>';
+      }
+    }
+  };
+  toptions: Options = {
+    floor: 1,
+    ceil: 30,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return value + '<b>Yr</b>';
+        case LabelType.High:
+          return value + '<b>Yr</b>';
+        default:
+          return value + '<b>Yr</b>';
+      }
+    }
+  };
+  moptions: Options = {
+    floor: 1,
+    ceil: 360,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return value + '<b>Mo</b>';
+        case LabelType.High:
+          return value + '<b>Mo</b>';
+        default:
+          return value + '<b>Mo</b>';
+      }
+    }
+  };
+  constructor() { 
+    this.yrToggel = true;
+  }
 
   ngOnInit(): void {
   }
-  getAnswer(){
-    this.input3=60*(this.input2*this.input1);
-
+  ngAfterViewInit() {
+    this.update();
   }
+
+  tbupdate(id: number) {
+    if (id == 0) {
+      this.pemi.value = (Number(this.query.amount) / 100000).toString();
+    }
+    else if (id == 1) {
+      this.remi.value = this.query.interest;
+    }
+    else if (id == 2) {
+      this.temi.value = this.query.tenureYr;
+    }
+    else if (id == 3) {
+      this.memi.value = this.query.tenureMo;
+    }
+    this.update();
+  }
+
+  update() {
+
+    var loanAmount = Number(this.pemi.value) * 100000;
+    var numberOfMonths = (this.yrToggel) ? (Number(this.temi.value) * 12) : Number(this.memi.value);
+    var rateOfInterest = Number(this.remi.value);
+    var monthlyInterestRatio = (rateOfInterest / 100) / 12;
+
+    this.query.amount = loanAmount.toString();
+    this.query.interest = rateOfInterest.toString();
+    if (this.yrToggel) {
+      this.query.tenureYr = this.temi.value.toString();
+    }
+    else {
+      this.query.tenureMo = this.memi.value.toString();
+    }
+
+    var top = Math.pow((1 + monthlyInterestRatio), numberOfMonths);
+    var bottom = top - 1;
+    var sp = top / bottom;
+    var emi = ((loanAmount * monthlyInterestRatio) * sp);
+    var full = numberOfMonths * emi;
+    var interest = full - loanAmount;
+    var int_pge = (interest / full) * 100;
+
+    this.result.emi = emi.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    var loanAmount_str = loanAmount.toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    this.result.total = full.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    this.result.interest = interest.toFixed(0).toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  // getAnswer(){
+
+  //   this.input3=60*(this.input2*this.input1);
+
+  // }
 
 }
