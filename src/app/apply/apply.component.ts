00ab319df5fcdf344c68ee3837a25508ba82c518
nithish1199 <nithish2011@gmail.com>
2021-09-28 12:58:03 +0530

    qwerty

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -7,74 +7,63 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./apply.component.css']
 })
 export class ApplyComponent implements OnInit {
-  PersonalDetails=new FormGroup({
-    firstName:new FormControl("",[Validators.required]),
-    lastName:new FormControl("",[Validators.required]),
-    middleName:new FormControl(),
-    Email:new FormControl("",[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    Password:new FormControl("", [Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}"),this.passwordMatchValidator]),
-    ConfirmPassword:new FormControl("", [Validators.required]),
-    MobileNumber:new FormControl("", [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
-    DateofBirth:new FormControl(),
-    Gender:new FormControl(),
-    Nationality:new FormControl(null,[Validators.required]),
-    AadharNumber:new FormControl(null,[Validators.required]),
-    PanNumber:new FormControl(null,[Validators.required]),
+  PersonalDetails = new FormGroup({
+    firstName: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    middleName: new FormControl(),
+    Email: new FormControl("", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    Password: new FormControl("", [Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")]),
+    ConfirmPassword: new FormControl("", [Validators.required]),
+    MobileNumber: new FormControl("", [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+    DateofBirth: new FormControl(),
+    Gender: new FormControl(),
+    Nationality: new FormControl(null, [Validators.required]),
+    AadharNumber: new FormControl(null, [Validators.required]),
+    PanNumber: new FormControl(null, [Validators.required]),
   }
-  //{validators:this.passwordMatchValidator}
+    //{validators:this.passwordMatchValidator}
   );
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  get firstname()
-  {
+  get firstname() {
     return this.PersonalDetails.get('firstName');
   }
 
-  get middlename()
-  {
+  get middlename() {
     return this.PersonalDetails.get('middleName');
   }
-  get lastname()
-  {
+  get lastname() {
     return this.PersonalDetails.get('lastName');
   }
-  get email()
-  {
+  get email() {
     return this.PersonalDetails.get('Email');
   }
-  get phonenumber()
-  {
+  get phonenumber() {
     return this.PersonalDetails.get('MobileNumber');
   }
-  get password()
-  {
+  get password() {
     return this.PersonalDetails.get('Password');
   }
-  get confirmpassword()
-  {
+  get confirmpassword() {
     return this.PersonalDetails.get('ConfirmPassword');
   }
-  get gender()
-  {
+  get gender() {
     return this.PersonalDetails.get('Gender');
   }
-  get nationality()
-  {
+  get nationality() {
     return this.PersonalDetails.get('Nationality');
   }
-  get adharnumber()
-  {
+  get adharnumber() {
     return this.PersonalDetails.get('AadharNumber');
   }
-  get pannumber()
-  {
+  get pannumber() {
     return this.PersonalDetails.get('PanNumber');
   }
 
-  passwordMatchValidator(PersonalDetails:FormGroup){
-    return PersonalDetails.controls['Password'].value===PersonalDetails!.controls['ConfirmPassword'].value?{'mismatch':false}:{'mismatch':true};
+  passwordMatchValidator(PersonalDetails: FormGroup) {
+    return PersonalDetails.controls['Password'].value === PersonalDetails!.controls['ConfirmPassword'].value ? { 'mismatch': false } : { 'mismatch': true };
   }
 }
