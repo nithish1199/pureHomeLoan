c1b49452e0a2771e7d865b2ae4014a50ef75bcc0
nithish1199 <nithish2011@gmail.com>
2021-09-29 17:59:19 +0530

    docupload

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PersonaldetailsService } from '../personaldetails.service';
 
 @Component({
   selector: 'app-documents',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
-
-  constructor() { }
-
+  selectedFile!: File;
+  constructor(private http: HttpClient, private service: PersonaldetailsService) { }
+  username:string="Nithish";
   ngOnInit(): void {
+  }
+  onFileSelected(event: any) {
+    this.selectedFile = <File>event.target.files[0];
+    console.log('hi')
+    console.log(event)
+  }
+  onUpload() {
+    const filedata = new FormData();
+    filedata.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:3751/api/documents/'+this.username, filedata)
+      .subscribe(res => {
+        console.log(res);
+      })
+
   }
 
 }
--- src/app/personaldetails.service.ts
+++ src/app/personaldetails.service.ts
@@ -50,6 +50,9 @@ export class PersonaldetailsService {
     
   } 
 
+
+
+
   errorHandler(error: { error: { message: string; }; status: any; message: any; }) {
     let errorMessage = '';
     if(error.error instanceof ErrorEvent) {
