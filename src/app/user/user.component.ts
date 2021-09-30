e13ea918af59a539d3033e5a695838ec925811a9
Soumitra2903 <Soumitra.Bandyopdhyay@lntinfotech.com>
2021-09-30 17:20:57 +0530

    changes made

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-<<<<<<< HEAD
-import { PersonaldetailsService } from '../personaldetails.service';
-import { Router,ActivatedRoute } from '@angular/router';
-=======
 import { Personaldetails } from '../personaldetails';
 import { Router } from '@angular/router';
 import { PersonaldetailsService } from '../personaldetails.service';
 import { ConstantPool } from '@angular/compiler';
 
->>>>>>> b7f1a09d1d16374ba14fd7cff77246dffe0f8183
 
 @Component({
   selector: 'app-user',
@@ -24,20 +19,6 @@ export class UserComponent implements OnInit {
     Username:new FormControl(),
     Password:new FormControl()
   });
-<<<<<<< HEAD
-  constructor(private service:PersonaldetailsService, private route:Router) { }
-
-  ngOnInit(): void {
-  }
-  msg!:string
-  
-  Login(){
-    console.log(this.LoginForm)
-    this.service.Login(this.LoginForm.value).subscribe(data=>{ console.log(data)},error=>this.msg="Invalid Login!") 
-    this.route.navigate(['userdash/username',{username:this.LoginForm.value.Username}]);
-     }
-}
-=======
   constructor(private service:PersonaldetailsService,private router:Router) { }
 
   ngOnInit(): void {
@@ -61,4 +42,3 @@ export class UserComponent implements OnInit {
   }
 
 }
->>>>>>> b7f1a09d1d16374ba14fd7cff77246dffe0f8183
